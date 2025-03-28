34f5cba0226c3b82800cd102973ea3b016f48e7e
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2025-03-27 23:36:06 -0400

    March 27 POTD - Medium

---
+++ 2780_Minimum_Index_of_a_Valid_Split.py
@@ -0,0 +1,15 @@
+
+class Solution:
+    def minimumIndex(self, nums: List[int]) -> int:
+        freq = Counter(nums)
+        maxElem = max(freq, key=freq.get)
+        maxFreq = freq[maxElem]
+
+        cnt = 0
+        for i, num in enumerate(nums):
+            if num == maxElem:
+                cnt += 1
+            remainingCnt = maxFreq - cnt
+            if cnt * 2 > (i + 1) and remainingCnt * 2 > (len(nums) - i - 1):
+                return i
+        return -1

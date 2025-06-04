ffe2796e0c579606b222436f522c85622a3b7ee7
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2025-06-03 22:07:19 -0400

    June 4 POTD - Medium

---
+++ 3403_Find_the_Lexicographically_Largest_String_From_the_Box_I.py
@@ -0,0 +1,14 @@
+
+class Solution:
+    def answerString(self, word: str, numFriends: int) -> str:
+        if numFriends == 1:
+            return word
+        
+        n = len(word)
+        res = ""
+        
+        for i in range(n):
+            minLen = min(n - numFriends + 1, n - i)
+            res = max(res, word[i:i + minLen])
+        
+        return res
